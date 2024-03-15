214c86b2e4d5eff5b81cd643b124b2a7e6085299
Pedro Manse <pedro.manse@dmk3.com.br>
2024-03-14 23:47:41 -0300

    support for devaps/tmpl as symlinks for $HOME/Templates/tmpl-rs/

---
+++ tmpl-rs/rust.py
@@ -0,0 +1,23 @@
+from sys import argv
+from os import system as exec, chdir
+def CMD(cmd):
+	print(f"[CMD]: {cmd}")
+	exec(cmd)
+
+argv.pop(0)
+projname = argv.pop(0)
+crates: list[tuple[str, list[str]]] = []
+
+for arg in argv:
+	if arg.startswith('-'):
+		crates[-1][1].append(arg[1:])
+	else:
+		crates.append((arg, []))
+
+CMD(f"cargo new --bin {projname}")
+chdir(projname)
+for (crate, features) in crates:
+	if features:
+		CMD(f"cargo add {crate} --features {' '.join(features)}")
+	else:
+		CMD(f"cargo add {crate}")
---
+++ tmpl-rs/rust.sh
@@ -0,0 +1,3 @@
+projname=$1
+#TODO: check for TMPL_RS env var
+python3 $HOME/Templates/tmpl-rs/rust.py $@
