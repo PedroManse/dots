99d55d47b3bbcc961b95cd85c604bf1dcda92ace
Pedro Manse <pedro.manse@dmk3.com.br>
2025-01-27 22:36:41 -0300

    nix/sys: removed git and neovim

--- nix/configuration.nix
+++ nix/configuration.nix
@@ -72,51 +72,21 @@
     alsa.enable = true;
     alsa.support32Bit = true;
     pulse.enable = true;
-    # If you want to use JACK applications, uncomment this
-    #jack.enable = true;
-
-    # use the example session manager (no others are packaged yet so this is enabled by default,
-    # no need to redefine it in your config for now)
-    #media-session.enable = true;
   };
 
-  # Enable touchpad support (enabled default in most desktopManager).
-  # services.xserver.libinput.enable = true;
-
-  # Define a user account. Don't forget to set a password with ‘passwd’.
   users.users.manse = {
     isNormalUser = true;
     description = "pedro manse";
     extraGroups = [ "networkmanager" "wheel" ];
-    #packages = with pkgs; [
-    #  kdePackages.kate
-    #  rustup
-    #  discord
-    #  alacritty
-    #  gh
-    #];
   };
 
-  #home-manager.users.manse = { pkgs, ... }: {
-  #  home.packages = with pkgs; [  ];
-  #  # The state version is required and should stay at the version you originally installed.
-  #  home.stateVersion = "24.11";
-  #};
-
-  # Install firefox.
+  # Install system-wide
   programs = {
     openvpn3.enable = true;
     hyprland.enable = true;
     firefox.enable = true;
-    neovim = {
-      enable = true;
-      defaultEditor = true;
-    };
     kdeconnect.enable = true;
     steam.enable = true;
-    git = {
-      enable = true;
-    };
     nix-ld = {
       enable = true;
       libraries = with pkgs; [
@@ -139,31 +109,6 @@
     hyprpaper
   ];
 
-  # Some programs need SUID wrappers, can be configured further or are
-  # started in user sessions.
-  # programs.mtr.enable = true;
-  # programs.gnupg.agent = {
-  #   enable = true;
-  #   enableSSHSupport = true;
-  # };
-
-  # List services that you want to enable:
-
-  # Enable the OpenSSH daemon.
-  # services.openssh.enable = true;
-
-  # Open ports in the firewall.
-  # networking.firewall.allowedTCPPorts = [ ... ];
-  # networking.firewall.allowedUDPPorts = [ ... ];
-  # Or disable the firewall altogether.
-  # networking.firewall.enable = false;
-
-  # This value determines the NixOS release from which the default
-  # settings for stateful data, like file locations and database versions
-  # on your system were taken. It‘s perfectly fine and recommended to leave
-  # this value at the release version of the first install of this system.
-  # Before changing this value read the documentation for this option
-  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
-  system.stateVersion = "24.11"; # Did you read the comment?
-
+  # original NixOS version; DO *NOT* ALTER
+  system.stateVersion = "24.11";
 }
