9dca351c6fea174fe98bd971ae96c74380b1f3bd
Pedro Manse <pedro.manse@dmk3.com.br>
2025-02-28 13:26:57 -0300

    moved backgrounds dir from dots/nix to dots/

--- nix/programs/wpaperd.nix
+++ nix/programs/wpaperd.nix
@@ -2,7 +2,7 @@
 	inherit enable;
 	settings = {
 		default = {
-			path = ../backgrounds; # send to /nix/store
+			path = ../../backgrounds; # send to /nix/store
 			duration = "1d";
 			queue-size = 3;
 		};
