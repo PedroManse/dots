df950916791865e2d9ea5e34f576ab89afc8b5e3
Pedro Manse <pedro.manse@dmk3.com.br>
2025-03-25 17:15:18 -0300

    nixfmt

--- nix/configuration.nix
+++ nix/configuration.nix
@@ -1,115 +1,122 @@
-{ config, pkgs, ... }: {
-	imports = [
-		# Include the results of the hardware scan.
-		/etc/nixos/hardware-configuration.nix
-		# Include home-manager
-		/home/manse/dots/nix/home.nix
-	];
+{ config, pkgs, ... }:
+{
+  imports = [
+    # Include the results of the hardware scan.
+    /etc/nixos/hardware-configuration.nix
+    # Include home-manager
+    /home/manse/dots/nix/home.nix
+  ];
 
-	nix.settings.experimental-features = [ "nix-command" "flakes" ];
-	# Bootloader.
-	boot.loader.systemd-boot.enable = true;
-	boot.loader.efi.canTouchEfiVariables = true;
+  nix.settings.experimental-features = [
+    "nix-command"
+    "flakes"
+  ];
+  # Bootloader.
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
 
-	# docker
-	#virtualisation.docker.enable = true;
+  # docker
+  #virtualisation.docker.enable = true;
 
-	networking.hostName = "manse-nix"; # Define your hostname.
+  networking.hostName = "manse-nix"; # Define your hostname.
 
-	# Enable networking
-	networking.networkmanager.enable = true;
+  # Enable networking
+  networking.networkmanager.enable = true;
 
-	# Set your time zone.
-	time.timeZone = "America/Sao_Paulo";
+  # Set your time zone.
+  time.timeZone = "America/Sao_Paulo";
 
-	# Select internationalisation properties.
-	i18n.defaultLocale = "en_US.UTF-8";
+  # Select internationalisation properties.
+  i18n.defaultLocale = "en_US.UTF-8";
 
-	i18n.extraLocaleSettings = {
-		LC_ADDRESS = "pt_BR.UTF-8";
-		LC_IDENTIFICATION = "pt_BR.UTF-8";
-		LC_MEASUREMENT = "pt_BR.UTF-8";
-		LC_MONETARY = "pt_BR.UTF-8";
-		LC_NAME = "pt_BR.UTF-8";
-		LC_NUMERIC = "pt_BR.UTF-8";
-		LC_PAPER = "pt_BR.UTF-8";
-		LC_TELEPHONE = "pt_BR.UTF-8";
-		LC_TIME = "pt_BR.UTF-8";
-	};
+  i18n.extraLocaleSettings = {
+    LC_ADDRESS = "pt_BR.UTF-8";
+    LC_IDENTIFICATION = "pt_BR.UTF-8";
+    LC_MEASUREMENT = "pt_BR.UTF-8";
+    LC_MONETARY = "pt_BR.UTF-8";
+    LC_NAME = "pt_BR.UTF-8";
+    LC_NUMERIC = "pt_BR.UTF-8";
+    LC_PAPER = "pt_BR.UTF-8";
+    LC_TELEPHONE = "pt_BR.UTF-8";
+    LC_TIME = "pt_BR.UTF-8";
+  };
 
-	# postgres
-	services.postgresql.enable = true;
+  # postgres
+  services.postgresql.enable = true;
 
-	# login/display manager
-	services.displayManager.ly.enable = true;
+  # login/display manager
+  services.displayManager.ly.enable = true;
 
-	services.xserver.xkb = {
-		layout = "br";
-		variant = "";
-	};
+  services.xserver.xkb = {
+    layout = "br";
+    variant = "";
+  };
 
-	# gpg agent + pin entry
-	services.pcscd.enable = true;
-	programs.gnupg.agent = {
-		enable = true;
-		pinentryPackage = pkgs.pinentry-curses;
-		enableSSHSupport = true;
-	};
+  # gpg agent + pin entry
+  services.pcscd.enable = true;
+  programs.gnupg.agent = {
+    enable = true;
+    pinentryPackage = pkgs.pinentry-curses;
+    enableSSHSupport = true;
+  };
 
-	# Configure console keymap
-	console.keyMap = "br-abnt2";
+  # Configure console keymap
+  console.keyMap = "br-abnt2";
 
-	# Enable CUPS to print documents.
-	# services.printing.enable = true;
+  # Enable CUPS to print documents.
+  # services.printing.enable = true;
 
-	# Enable sound with pipewire.
-	hardware.pulseaudio.enable = false;
-	security.rtkit.enable = true;
-	services.pipewire = {
-		enable = true;
-		alsa.enable = true;
-		alsa.support32Bit = true;
-		pulse.enable = true;
-	};
+  # Enable sound with pipewire.
+  hardware.pulseaudio.enable = false;
+  security.rtkit.enable = true;
+  services.pipewire = {
+    enable = true;
+    alsa.enable = true;
+    alsa.support32Bit = true;
+    pulse.enable = true;
+  };
 
-	users.users.manse = {
-		isNormalUser = true;
-		description = "pedro manse";
-		# extraGroups = [ "networkmanager" "wheel" "docker" ]; # network, sudo, docker
-		extraGroups = [ "networkmanager" "wheel" ];
-	};
+  users.users.manse = {
+    isNormalUser = true;
+    description = "pedro manse";
+    # extraGroups = [ "networkmanager" "wheel" "docker" ]; # network, sudo, docker
+    extraGroups = [
+      "networkmanager"
+      "wheel"
+    ];
+  };
 
-	programs = {
-		hyprland.enable = true;
-		firefox.enable = true;
-		steam.enable = true;
-		nix-ld = {
-			enable = true;
-			libraries = with pkgs; [
-				libgcc
-			];
-		};
-	};
+  programs = {
+    hyprland.enable = true;
+    firefox.enable = true;
+    steam.enable = true;
+    nix-ld = {
+      enable = true;
+      libraries = with pkgs; [
+        libgcc
+      ];
+    };
+  };
 
-# Allow unfree packages
-	nixpkgs.config.allowUnfree = true;
-	nix.extraOptions = ''
-keep-outputs = true
-keep-derivations = true
-	'';
+  # Allow unfree packages
+  nixpkgs.config.allowUnfree = true;
+  nix.extraOptions = ''
+    keep-outputs = true
+    keep-derivations = true
+    	'';
 
-	environment.systemPackages = with pkgs; [
-		pulseaudio
-		man-pages
-		man-pages-posix
-		pinentry-curses
-		pavucontrol
-		gcc14
-		hyprshot
-		hyprlock
-		hyprcursor
-	];
+  environment.systemPackages = with pkgs; [
+    pulseaudio
+    man-pages
+    man-pages-posix
+    pinentry-curses
+    pavucontrol
+    gcc14
+    hyprshot
+    hyprlock
+    hyprcursor
+  ];
 
-	# original NixOS version; DO *NOT* ALTER
-	system.stateVersion = "24.11";
+  # original NixOS version; DO *NOT* ALTER
+  system.stateVersion = "24.11";
 }
--- nix/home.nix
+++ nix/home.nix
@@ -1,63 +1,64 @@
 { config, pkgs, ... }:
 let
-	home-manager = builtins.fetchTarball "https://github.com/nix-community/home-manager/archive/release-24.11.tar.gz";
-	p = import ./autoprogs.nix ./programs;
+  home-manager = builtins.fetchTarball "https://github.com/nix-community/home-manager/archive/release-24.11.tar.gz";
+  p = import ./autoprogs.nix ./programs;
 in
 {
-	imports = [
-		(import "${home-manager}/nixos")
-	];
+  imports = [
+    (import "${home-manager}/nixos")
+  ];
 
-	home-manager.users.manse = {pkgs, ...}: {
-		nixpkgs.config.allowUnfree = true;
-		home = {
-			pointerCursor = {
-				name = "rose-pine-cursor";
-				size = 24;
-				x11.enable = true;
-				gtk.enable = true;
-				package = pkgs.rose-pine-cursor;
-			};
-			stateVersion = "24.11";
-			packages = with pkgs; [
-        nixfmt-rfc-style
-				typescript-language-server
-				bitwarden-desktop
-				qbittorrent
-				rustup
-				vlc
-				discord
-			];
-		};
+  home-manager.users.manse =
+    { pkgs, ... }:
+    {
+      nixpkgs.config.allowUnfree = true;
+      home = {
+        pointerCursor = {
+          name = "rose-pine-cursor";
+          size = 24;
+          x11.enable = true;
+          gtk.enable = true;
+          package = pkgs.rose-pine-cursor;
+        };
+        stateVersion = "24.11";
+        packages = with pkgs; [
+          nixfmt-rfc-style
+          typescript-language-server
+          bitwarden-desktop
+          qbittorrent
+          rustup
+          vlc
+          discord
+        ];
+      };
 
-		gtk = {
-			gtk3 = {
-				extraConfig.gtk-application-prefer-dark-theme = true;
-			};
-		};
+      gtk = {
+        gtk3 = {
+          extraConfig.gtk-application-prefer-dark-theme = true;
+        };
+      };
 
-		wayland.windowManager.hyprland = p.hyprland {};
+      wayland.windowManager.hyprland = p.hyprland { };
 
-		programs = {
-			eza = p.eza {};
-			alacritty = p.alacritty {};
-			git = p.git {};
-			wofi = p.wofi {};
-			direnv = p.direnv {};
-			neovim = p.neovim {};
-			wpaperd = p.wpaperd {};
+      programs = {
+        eza = p.eza { };
+        alacritty = p.alacritty { };
+        git = p.git { };
+        wofi = p.wofi { };
+        direnv = p.direnv { };
+        neovim = p.neovim { };
+        wpaperd = p.wpaperd { };
 
-			bat.enable = true;
-			gh.enable = true;
-			waybar.enable = true;
+        bat.enable = true;
+        gh.enable = true;
+        waybar.enable = true;
 
-
-			bash = {
-				enable = true;
-				bashrcExtra = ''
-					. /home/manse/dots/bash/bashrc
-				'';
-			};
-		};
-	};
+        bash = {
+          enable = true;
+          bashrcExtra = ''
+            					. /home/manse/dots/bash/bashrc
+            				'';
+        };
+      };
+    };
 }
