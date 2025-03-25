c01780d316e30485bcaff2d47f9a6a1e39259597
Pedro Manse <pedro.manse@dmk3.com.br>
2025-03-25 17:15:58 -0300

    nvim/config: standard quotes

--- nvim/lua/config/init.lua
+++ nvim/lua/config/init.lua
@@ -1,4 +1,4 @@
-require("config.lsp") -- ./lsp.lua
+require('config.lsp') -- ./lsp.lua
 require('config.maps') -- ./maps.lua
 require('config.options') -- ./options.lua
 require('config.vim') -- ./vim.lua
