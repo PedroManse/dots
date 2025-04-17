bdf976de027278881d82c364d60e7cc56d0be09f
Pedro Manse <pedro.manse@dmk3.com.br>
2025-04-17 13:06:47 -0300

    nix tmpls; if arg in nix-shells, use that, else default behaviour

---
+++ tmpl-rs/nix-shells/prisma.nix
@@ -0,0 +1,22 @@
+{
+  pkgs ? import <nixpkgs> { },
+}:
+
+pkgs.mkShellNoCC {
+  buildInputs = with pkgs; [
+    nodejs_23
+    openssl
+    nodePackages_latest.prisma
+    typescript
+  ];
+
+  shellHook = ''
+    		export PATH="./node_modules/bin:$PATH"
+    	'';
+
+  env = {
+    PRISMA_QUERY_ENGINE_LIBRARY = "${pkgs.prisma-engines}/lib/libquery_engine.node";
+    PRISMA_QUERY_ENGINE_BINARY = "${pkgs.prisma-engines}/bin/query-engine";
+    PRISMA_SCHEMA_ENGINE_BINARY = "${pkgs.prisma-engines}/bin/schema-engine";
+  };
+}
--- tmpl-rs/nixshell.sh
+++ tmpl-rs/nixshell.sh
@@ -1,15 +1,29 @@
 #! /usr/bin/env bash
 
-echo "{
-  pkgs ? import <nixpkgs> { },
-}:
-pkgs.mkShellNoCC {
-  name = \"dev-shell\";
-  packages = with pkgs; [ $* ];
+write_custom_shell() {
+	echo "{
+		pkgs ? import <nixpkgs> { },
+	}:
+	pkgs.mkShellNoCC {
+		name = \"dev-shell\";
+		packages = with pkgs; [ $* ];
+	}
+	" > shell.nix
+	nixfmt shell.nix
 }
-" > shell.nix
+
+write_tmpl_shell() {
+	cp $1 ./shell.nix
+}
+
+if [ -f "$TMPLRS_DIR/nix-shells/$1.nix" ] ; then
+	write_tmpl_shell "$TMPLRS_DIR/nix-shells/$1.nix"
+else
+	write_custom_shell $@
+fi
+
 echo "use nix" > .envrc
 if [ git status &> /dev/null ] ; then
-  echo ".direnv" >> .gitignore
+	echo ".direnv" >> .gitignore
 fi
-nixfmt shell.nix
+
