976a0da64492a2886a906f14c672a99fca43d91c
Pedro Manse <pedro.manse@dmk3.com.br>
2025-05-06 09:46:44 -0300

    nix/git/delta: enable side-by-size and zdiff3

--- nix/programs/git.nix
+++ nix/programs/git.nix
@@ -1,39 +1,45 @@
-{enable?true}: {
-	inherit enable;
-	userName = "Pedro Manse";
-	userEmail = "pedro.manse@dmk3.com.br";
-	aliases = {
-		f = "fetch --prune";
-		c = "checkout";
-		b = "branch";
-	};
-	delta = {
-		enable = true;
-	};
-	extraConfig = {
-		safe = {
-			directory = ".";
-		};
-		commit = {
-			gpgsign = true;
-		};
-		merge = {
-			conflictstyle = "diff3";
-		};
-		core = {
-			editor = "nvim";
-		};
-		diff = {
-			colorMoved = false;
-		};
-		# defined in ~/.gitconfig; not here
-		#user = { signingkey = ""; };
-		push = {
-			autoSetupRemote = true;
-			default = "current";
-		};
-		credential = {
-			helper = "!/usr/bin/gh auth git-credential";
-		};
-	};
+{
+  enable ? true,
+}:
+{
+  inherit enable;
+  userName = "Pedro Manse";
+  userEmail = "pedro.manse@dmk3.com.br";
+  aliases = {
+    f = "fetch --prune";
+    c = "checkout";
+    b = "branch";
+  };
+  delta = {
+    enable = true;
+    options = {
+      side-by-side = true;
+    };
+  };
+  extraConfig = {
+    safe = {
+      directory = ".";
+    };
+    commit = {
+      gpgsign = true;
+    };
+    merge = {
+      conflictstyle = "zdiff3";
+    };
+    core = {
+      editor = "nvim";
+    };
+    diff = {
+      colorMoved = false;
+    };
+    # defined in ~/.gitconfig; not here
+    #user = { signingkey = ""; };
+    push = {
+      autoSetupRemote = true;
+      default = "current";
+    };
+    credential = {
+      helper = "!/usr/bin/gh auth git-credential";
+    };
+  };
 }
