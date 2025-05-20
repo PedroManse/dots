7f9447b901d6104f64b850ecad14133d2da221a4
Pedro Manse <pedro.manse@dmk3.com.br>
2025-05-20 00:40:45 -0300

    alacritty: 2px padding, mononoki nerd font

--- nix/programs/alacritty.nix
+++ nix/programs/alacritty.nix
@@ -4,8 +4,15 @@
 {
   inherit enable;
   settings = {
+    window = {
+      padding = {
+        x = 2;
+        y = 2;
+      };
+    };
     font = {
-      normal.family = "mononoki";
+      #normal.family = "mononoki";
+      normal.family = "Mononoki Nerd Font";
       size = 9.6;
     };
     colors.primary.background = "#000000";
