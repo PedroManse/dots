6107ce66382cb76e3f5b560e2efc2998ec319f65
Pedro Manse <pedro.manse@dmk3.com.br>
2025-06-24 11:34:28 -0300

    bash/nvim: open file with <file>:<line> opens at line number

--- bash/nvim.bash
+++ bash/nvim.bash
@@ -1,12 +1,37 @@
+# open file on host vim
 _nvim_con() {
 	abs_path=$(readlink --canonicalize "$@" | sed s'| |\\ |'g)
 	$(get_bin_path nvim) --server $NVIM --remote-send "<ESC>:edit $abs_path<CR>"
 	exit
 }
 
+# start host and open file
+_nvim_srv() {
+	$(get_bin_path nvim) --listen $HOME/.cache/nvim/$$-server.pipe $@
+}
+
 if [ -n "$NVIM" ] ; then
 	export EDITOR="_nvim_con"
 else
-	export EDITOR="$(get_bin_path nvim) --listen $HOME/.cache/nvim/$$-server.pipe"
+	export EDITOR="_nvim_srv"
 fi
-alias nvim="$EDITOR"
+
+_open() {
+	path_parts=$(readlink --canonicalize "$@" | sed s'| |\\ |'g | sed 's/:/\t/' )
+	file=$(echo "$path_parts" | awk ' { print $1 }' )
+	line=$(echo "$path_parts" | awk ' { print $2 }' )
+
+	if [ -n "$line" ] ; then
+		# has line number
+		if [ -n "$NVIM" ] ; then
+			$(get_bin_path nvim) --server $NVIM --remote-send "<ESC>:edit $file<CR>:+$line<CR>"
+			exit
+		else
+			$(get_bin_path nvim) --listen $HOME/.cache/nvim/$$-server.pipe $file "+:$line"
+		fi
+	else
+		$EDITOR $file
+	fi
+}
+
+alias nvim="_open"
