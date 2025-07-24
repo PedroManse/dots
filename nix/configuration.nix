36464ea842fecc617d75bd880e18614d5555de14
Pedro Manse <pedro.manse@dmk3.com.br>
2025-07-24 19:53:16 -0300

    nixos: mpd daemon

--- nix/configuration.nix
+++ nix/configuration.nix
@@ -70,6 +70,19 @@
       variant = "";
     };
 
+    mpd = {
+      enable = true;
+      musicDirectory = "/var/music";
+      extraConfig = ''
+        audio_output {
+          type "pipewire"
+          name "mpd pipewire"
+        }
+
+      '';
+
+    };
+
   };
 
   # gpg agent + pin entry
