a8ac04306bdd00dbbc210152970a33027ff44738
Manse <pedro.manse@dmk3.com.br>
2025-08-17 00:14:41 -0300

    bash: show nix logo if in nix shell + more colors

--- bash/pwd.bash
+++ bash/pwd.bash
@@ -1,16 +1,16 @@
 format_dir() {
 	thisdir=$1
 	# if fpwd-daemon is running, use it
-	fdir=$(echo $thisdir | socat - UNIX-CONNECT:/run/fpwd-rs.sock 2> /dev/null)
+	fdir=$(echo "$thisdir" | socat - UNIX-CONNECT:/run/fpwd-rs.sock 2> /dev/null)
 	if [ $? = 0 ] ; then
-		echo $fdir
+		echo "$fdir"
 	else
 		# else, try pwd-rs
 		if fdir=$(PWD=$thisdir "pwd-rs") ; then
-			echo $fdir
+			echo "$fdir"
 		# else, just don't format path
 		else
-			echo $thisdir
+			echo "$thisdir"
 		fi
 	fi
 }
@@ -21,12 +21,17 @@ PROMPT_COMMAND=prompt_command
 prompt_command() {
 	e=$?
 	# print formatted working path
-	fdir=$(format_dir $PWD)
+	fdir=$(format_dir "$PWD")
 	PS="${fdir}$(col_reset)"
 
 	# in case of error, show code
 	if [[ $e != '0' ]] ; then
 		PS="${PS} [$(col_bold)$(col_underline)$(col_reverse)$(col_red)$e$(col_reset)]"
+	fi
+
+	# show nix logo if in nix shell
+	if [ -n "$IN_NIX_SHELL" ] ; then
+		PS="$PS $(col_cyan) $(col_reset)"
 	fi
 
 	# show git status
@@ -35,20 +40,19 @@ prompt_command() {
 	fi
 
 	# show computer name if it's not "."
-	if [ -n "$COMPUTER_NAME" -a "$COMPUTER_NAME" != "." ] ; then
+	if [ -n "$COMPUTER_NAME" ] && [ "$COMPUTER_NAME" != "." ] ; then
 		PS="$COMPUTER_NAME: ${PS}"
 	fi
 
 	# pushd/popd list
 	dirlist=$(dirs)
 	dircount=$(echo "$dirlist" | wc -w)
-	stack=$(($dircount))
 	comdirs=""
-	for i in $(seq 2 $stack); do
+	for i in $(seq 2 "$dircount"); do
 		d='$'
 		d+="$i"
-		thisdir=$(echo $dirlist | awk "{printf $d }")
-		thisdir=$(format_dir $thisdir)
+		thisdir=$(echo "$dirlist" | awk "{printf $d }")
+		thisdir=$(format_dir "$thisdir")
 		comdirs+=$thisdir
 		comdirs+="$(col_reset) | "
 	done
