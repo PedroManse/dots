64a812303da3f59462fbf369cd548d0b3a7ba6bf
Manse <pedro.manse@dmk3.com.br>
2025-08-17 00:16:51 -0300

    nix/progs/coding: bash + emmet lsp

--- nix/programs/coding.nix
+++ nix/programs/coding.nix
@@ -1,9 +1,13 @@
 pkgs: with pkgs; [
+  # LSPs
   nixd
+  bash-language-server
   typescript-language-server
   lua-language-server
+  emmet-language-server
+
+  # others
   nixfmt-rfc-style
   shellcheck
   rustup
-  emmet-language-server
 ]
