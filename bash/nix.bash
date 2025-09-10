1d1476d28f01f467c99fffee9aa32378ccc8582b
Manse <pedro.manse@dmk3.com.br>
2025-09-10 00:39:29 -0300

    command completion for nix-edit

--- bash/nix.bash
+++ bash/nix.bash
@@ -1,8 +1,10 @@
 alias nix-switch="sudo nixos-rebuild switch"
+
 nix-clean() {
 	nix-collect-garbage --delete-old
 	sudo nix-collect-garbage -d
 }
+
 nix-edit() {
 	case $1 in
 	"progs" | "prog")
@@ -27,6 +29,25 @@ commands:
 		;;
 	esac
 }
+
+__nix_edit_complete() {
+	if [ ${#COMP_WORDS[@]} -gt 3 ] ; then
+		return
+	fi
+
+	if [ "${3%s}" = "prog" ] ; then
+		CR=""
+		for path in "$DOTS"/nix/programs/*.nix ; do
+			file=$(basename "$path")
+			CR="$CR ${file%.nix}"
+		done
+		COMPREPLY=($(compgen -W "$CR" "${COMP_WORDS[2]}" ))
+	else
+		COMPREPLY=($(compgen -W "sys home progs" "${COMP_WORDS[1]}" ))
+	fi
+}
+complete -F __nix_edit_complete "nix-edit"
+
 alias nix-ehome="\$EDITOR \$DOTS/nix/home.nix"
 alias nix-esys="\$EDITOR \$DOTS/nix/configuration.nix"
 
