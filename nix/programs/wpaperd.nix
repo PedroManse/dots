e3931a20de830c6a64484df71c30726a8bcc9e55
Manse <pedro.manse@dmk3.com.br>
2025-09-12 17:39:36 -0300

    hidden dir for backgrounds
    constant completion conflict with bash dir

--- nix/programs/wpaperd.nix
+++ nix/programs/wpaperd.nix
@@ -5,7 +5,7 @@
   inherit enable;
   settings = {
     default = {
-      path = ../../backgrounds; # send to /nix/store
+      path = ../../.backgrounds; # send to /nix/store
       duration = "1d";
       queue-size = 3;
     };
