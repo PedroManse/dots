b21471d9ccb2b330c177065cc5ef9eaf3cfbce53
Manse <pedro.manse@dmk3.com.br>
2025-09-15 23:45:48 -0300

    bash/nvim: fixed opening last file
    was quoting file variable, so was opening nvim with '' arg

--- bash/nvim.bash
+++ bash/nvim.bash
@@ -1,17 +1,17 @@
 # open file on host vim
 _nvim_con() {
 	if [ ! $# = 0 ] ; then
-		abs_path=$(readlink --canonicalize "$@" | sed s'| |\\ |'g)
-		$(get_bin_path nvim) --server $NVIM --remote-send "<ESC>:edit $abs_path<CR>"
+		abs_path=$(readlink --canonicalize "$@" | sed 's| |\\ |g')
+		$(get_bin_path nvim) --server "$NVIM" --remote-send "<ESC>:edit $abs_path<CR>"
 	else
-		$(get_bin_path nvim) --server $NVIM --remote-send "<ESC>:enew<CR>"
+		$(get_bin_path nvim) --server "$NVIM" --remote-send "<ESC>:enew<CR>"
 	fi
 	exit
 }
 
 # start host and open file
 _nvim_srv() {
-	$(get_bin_path nvim) --listen $HOME/.cache/nvim/$$-server.pipe $@
+	$(get_bin_path nvim) --listen "$HOME"/.cache/nvim/$$-server.pipe $@
 }
 
 if [ -n "$NVIM" ] ; then
@@ -22,7 +22,7 @@ fi
 
 _open() {
 	if [ ! $# = 0 ] ; then
-		path_parts=$(readlink --canonicalize "$@" | sed s'| |\\ |'g | sed 's/:/\t/' )
+		path_parts=$(readlink --canonicalize "$@" | sed 's| |\\ |g' | sed 's/:/\t/' )
 		file=$(echo "$path_parts" | awk ' { print $1 }' )
 		line=$(echo "$path_parts" | awk ' { print $2 }' )
 	fi
@@ -30,10 +30,10 @@ _open() {
 	if [ -n "$line" ] ; then
 		# has line number
 		if [ -n "$NVIM" ] ; then
-			$(get_bin_path nvim) --server $NVIM --remote-send "<ESC>:edit $file<CR>:+$line<CR>"
+			$(get_bin_path nvim) --server "$NVIM" --remote-send "<ESC>:edit $file<CR>:+$line<CR>"
 			exit
 		else
-			$(get_bin_path nvim) --listen $HOME/.cache/nvim/$$-server.pipe $file "+:$line"
+			$(get_bin_path nvim) --listen "$HOME"/.cache/nvim/$$-server.pipe "$file" "+:$line"
 		fi
 	else
 		$EDITOR $file
