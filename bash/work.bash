001b0b39124f2ee42a83a4fb6c3e4a06ce063efd
Manse <pedro.manse@dmk3.com.br>
2025-10-03 11:29:00 -0300

    work: better script

--- bash/work.bash
+++ bash/work.bash
@@ -1,11 +1,17 @@
 #! /usr/bin/env bash
+DIARY_REPO="$HOME/diary"
+DEFAULT_LOG_NAME="work"
 
 function work {
+	pushd "$DIARY_REPO"
 	git pull
-	$EDITOR "$HOME/diary/work"
+	if [ ! -z "$1" ] ; then
+		$EDITOR "$1"
+	else
+		$EDITOR "$DEFAULT_LOG_NAME"
+	fi
 	gitmsg=$(date +'%d/%mT%H:%M')
-	pushd "$HOME/diary"
-	git commit -am "$gitmsg" -m "\nauto commit"
+	git commit -am "$gitmsg" -m "\nautomatic commit"
 	git push
 	popd
 }
