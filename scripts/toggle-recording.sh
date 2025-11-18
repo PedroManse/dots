44519ef152dcef6ce0b41f27dd921cdf64d0ef84
Manse <122305437+PedroManse@users.noreply.github.com>
2025-11-18 09:19:27 -0300

    scripts(record): capture default sink (mic)

--- scripts/toggle-recording.sh
+++ scripts/toggle-recording.sh
@@ -4,5 +4,5 @@ if pgrep "wf-recorder" ; then
 	pkill wf-recorder
 else
   now=$(date +'%d-%m_%H-%M.mp4')
-  wf-recorder -a "--file=$HOME/Screencasts/auto/$now"
+  wf-recorder "-a$(pactl get-default-sink)" "--file=$HOME/Screencasts/auto/$now"
 fi
