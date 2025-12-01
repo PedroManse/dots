82555e1cac92d85e03adc5a6da861ebfe7a27340
Manse <122305437+PedroManse@users.noreply.github.com>
2025-12-01 08:35:30 -0300

    bash(screencasts): convert to H265 codec with ffmpeg

---
+++ bash/screencasts.bash
@@ -0,0 +1,18 @@
+function default {
+	default=$1
+	optional=$2
+	if [ -z "$optional" ] ; then
+		echo "$default"
+	else
+		echo "$optional"
+	fi
+}
+
+function convert {
+	infile=$1
+	outfile=$2
+	framerate=$(default 24 "$3")
+	quality=$(default 28 "$4")
+	ffmpeg -i "$infile" -vcodec libx265 -crf 28 -r $framerate  "$outfile"
+}
+
