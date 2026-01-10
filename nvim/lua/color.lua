ff374ba045938eb64c8d9c05c18c0e6b4cc86d89
Manse <122305437+PedroManse@users.noreply.github.com>
2026-01-10 12:47:00 -0300

    neovim(color): remove gruber config

--- nvim/lua/color.lua
+++ nvim/lua/color.lua
@@ -1,10 +1,6 @@
 vim.o.syntax="enable"
 vim.o.winborder = 'rounded'
 
-require'gruber-darker'.setup{
-	background=false
-}
-
 -- zenburn
 vim.g.zenburn_force_dark_Background=1
 vim.g.zenburn_transparent=1
