5a116b82b24088029991fed560ad936b3995a1af
Manse <122305437+PedroManse@users.noreply.github.com>
2026-01-10 12:47:25 -0300

    bash: utils file + convert function

---
+++ bash/utils.bash
@@ -0,0 +1,29 @@
+_ct_to_ns="
+ns 1
+us 1000
+ms 1000000
+s  1000000000
+"
+
+function convert() {
+	table="$1"
+	from="$2"
+	required="$3"
+	got_it=0
+
+	export IFS=$'\n '
+	for line in $table ; do
+		if [ "$got_it" = 1 ] ; then
+			echo "$line"
+			return
+		fi
+		if [ "$from" = "$line" ] ; then
+			got_it=1
+		fi
+	done
+
+	if ! [ "$required" = "optional" ] ; then
+		echo "Can't map $from with table ($table)";
+		exit 1
+	fi
+}
