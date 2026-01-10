bb3e204b680e77aa7f39375e889a860bc6afb37f
Manse <122305437+PedroManse@users.noreply.github.com>
2026-01-10 12:48:40 -0300

    scripts(toggle-rec): exec _pwa_fix_outputs to record desktop and mic sound

--- scripts/toggle-recording.sh
+++ scripts/toggle-recording.sh
@@ -1,6 +1,9 @@
 #! /usr/bin/env bash
 
 if ! pkill -2 wf-recorder ; then
-  now=$(date +'%d-%m_%H:%M.mp4')
-  wf-recorder "--audio=virtualmic" "--file=$HOME/Screencasts/auto/$now"
+	source "/home/manse/.shenv.bash"
+	source "/home/manse/dots/bash/bashrc"
+	_pwa_fix_outputs
+	now=$(date +'%d-%m_%H:%M.mp4')
+	wf-recorder "--audio=virtualmic" "--file=$HOME/Screencasts/auto/$now"
 fi
