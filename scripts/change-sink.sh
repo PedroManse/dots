41ce62a06daabde80591a466ecc74a66b228c585
Manse <122305437+PedroManse@users.noreply.github.com>
2026-01-10 12:49:47 -0300

    scripts(change-sink): show named sinks + don't set -e on _pwa_rotate
    _pwa_rotate_real_output_sinks failed on first two uses; but with set -e it never normalized

--- scripts/change-sink.sh
+++ scripts/change-sink.sh
@@ -2,9 +2,9 @@
 
 source "/home/manse/.shenv.bash"
 source "/home/manse/dots/bash/bashrc"
-set -e
 _pwa_rotate_real_output_sinks
+set -e
 hyprctl dismissnotify
 hyprctl notify -1 3000 'rgb(000000)' "Current audio sink:
-\"$(_pwa_find_real_output)\""
+\"$(_pwa_find_real_named_output)\""
 
