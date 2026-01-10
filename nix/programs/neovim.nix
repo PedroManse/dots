ce2973d53f73deb29f430e1a194be86ee69e569c
Manse <122305437+PedroManse@users.noreply.github.com>
2026-01-10 12:45:57 -0300

    nix(neovim): remove gruber and alabaster themes

--- nix/programs/neovim.nix
+++ nix/programs/neovim.nix
@@ -24,19 +24,6 @@
     vim-prisma
     rust-vim
     undotree
-    gruber-darker-nvim
     zig-vim
-    (pkgs.vimUtils.buildVimPlugin {
-      pname = "alabaster.nvim";
-      version = "1.0.0";
-      src = pkgs.fetchFromGitHub {
-        owner = "p00f";
-        repo = "alabaster.nvim";
-        rev = "1fc9e29fbbce94f127cc8b21960b7e3c85187960";
-        sha256 = "Xng+shYT7BtrD6ZSnCGgt01lm9ZALfYwivYRGRjNpUo=";
-      };
-      meta.homepage = "https://github.com/p00f/alabaster.nvim";
-      meta.hydraPlatforms = [ ];
-    })
   ];
 }
