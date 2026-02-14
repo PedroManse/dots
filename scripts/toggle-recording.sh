f7b15cf435a855d731fc6328bc2ef93bbd744476
Manse <122305437+PedroManse@users.noreply.github.com>
2026-02-14 10:03:54 -0300

    scripts/toggle-recording show hyprland notification after recording and execute _pwa_fix_output

--- scripts/toggle-recording.sh
+++ scripts/toggle-recording.sh
@@ -1,9 +1,19 @@
 #! /usr/bin/env bash
 
+format_rec_time() {
+	printf '%d:%02d' $(( "$1" / 60 )) $(( "$1" % 60 ))
+}
+
 if ! pkill -2 wf-recorder ; then
 	source "/home/manse/.shenv.bash"
 	source "/home/manse/dots/bash/bashrc"
 	_pwa_fix_outputs
 	now=$(date +'%d-%m_%H:%M.mp4')
+	now_unix=$(date +'%s')
 	wf-recorder "--audio=virtualmic" "--file=$HOME/Screencasts/auto/$now"
+	end_unix=$(date +'%s')
+	diff_unix=$(( $end_unix - $now_unix ))
+	diff=$(format_rec_time $diff_unix)
+	hyprctl dismissnotify
+	hyprctl notify -1 3000 'rgb(000000)' "$diff @ ~/Screencasts/auto/$now"
 fi
