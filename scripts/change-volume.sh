d8009f8649526fe8f945e218743df7e317b7cae9
Manse <122305437+PedroManse@users.noreply.github.com>
2026-04-01 11:15:52 -0300

    scripts/change-volume: hyprland notification on volume change

--- scripts/change-volume.sh
+++ scripts/change-volume.sh
@@ -6,3 +6,5 @@ set -e
 sink=$(_pwa_find_real_output)
 pactl set-sink-volume "$sink" "$@"
 volume=$(pactl get-sink-volume "$sink" | awk '{print $5}')
+hyprctl dismissnotify
+hyprctl notify -1 1500 'rgb(000000)' "Current volume: $volume"
