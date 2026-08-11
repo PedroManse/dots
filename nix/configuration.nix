34f77f720ac13abf66206f0f5679fc2f1cb79b98
Manse <122305437+PedroManse@users.noreply.github.com>
2026-08-11 00:50:01 -0300

    nix: bluetooth support

--- nix/configuration.nix
+++ nix/configuration.nix
@@ -205,9 +205,31 @@
     w3m
   ];
 
-  fonts.packages = [
-    pkgs.nerd-fonts.mononoki
-  ];
+  fonts.packages = builtins.filter pkgs.lib.attrsets.isDerivation (
+    builtins.attrValues pkgs.nerd-fonts
+  );
+
+  hardware.bluetooth = {
+    enable = true;
+    powerOnBoot = true;
+    settings = {
+      General = {
+        # Shows battery charge of connected devices on supported
+        # Bluetooth adapters. Defaults to 'false'.
+        Experimental = true;
+        # When enabled other devices can connect faster to us, however
+        # the tradeoff is increased power consumption. Defaults to
+        # 'false'.
+        FastConnectable = true;
+      };
+      Policy = {
+        # Enable all controllers when they are found. This includes
+        # adapters present on start as well as adapters that are plugged
+        # in later on. Defaults to 'true'.
+        AutoEnable = true;
+      };
+    };
+  };
 
   # original NixOS version; DO *NOT* ALTER
   system.stateVersion = "25.11";
